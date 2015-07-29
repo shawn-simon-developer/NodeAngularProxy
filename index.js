a487da1d6a5749debea73a6a5694e8fcc9412c36
Shawn Simon <shawn.simon.developer@gmail.com>
2015-07-29 14:49:41 -0400

    Removed code that wasn't needed.

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
-/**
- * Module dependencies.
- */
 var express = require('express');
-var https = require('https');
-var path = require('path');
-var fs = require('fs');
+var httpProxy = require('http-proxy');
+var bodyParser = require('body-parser');
 
 var apiForwardingUrl = 'http://api.open-notify.org/astros.json?';
 
-
-var httpProxy = require('http-proxy');
-//Solution for forwarding from http to https taken from
-//http://stackoverflow.com/questions/15801014/how-to-use-node-http-proxy-for-http-to-https-routing
-var proxyOptions = {
-    changeOrigin: true
-};
-
-httpProxy.prototype.onError = function (err) {
-    console.log(err);
-};
-
-var apiProxy = httpProxy.createProxyServer(proxyOptions);
-
-// Node express server setup.
 var server = express();
 server.set('port', 3000);
 server.use(express.static(__dirname + '/app'));
 
+var apiProxy = httpProxy.createProxyServer();
+
 console.log('Forwarding API requests to ' + apiForwardingUrl);
 
 server.all("/space/*", function(req, res) {
-    console.log("Ye");
     apiProxy.web(req, res, {target: apiForwardingUrl});
 });
 
-var bodyParser = require('body-parser');
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// Start Server.
 server.listen(server.get('port'), function() {
     console.log('Express server listening on port ' + server.get('port'));
 });
